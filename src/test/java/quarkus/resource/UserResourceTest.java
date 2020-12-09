47cd2db08880eb7fc3b4b32caa0a08042bb1f4b2
Alexandre Guidin <alexandreguidin@gmail.com>
2020-12-09 14:10:19 -0300

    add yaml

--- src/test/java/quarkus/resource/UserResourceTest.java
+++ src/test/java/quarkus/resource/UserResourceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import quarkus.config.BaseResourceTest;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
@@ -37,5 +38,17 @@ public class UserResourceTest extends BaseResourceTest {
                 .body("document_number", is("123"))
                 .body("info.email", is("test@test.com"))
                 .body("info.name", is("Test"));
+    }
+
+    @Test
+    public void save_invalid() {
+        given()
+                .when().body("{}")
+                .contentType(ContentType.JSON)
+                .post("/user")
+                .then()
+                .statusCode(422)
+                .contentType(ContentType.JSON)
+                .body(matchesJsonSchemaInClasspath("json/resource/user/response/save_invalid_fields.json"));
     }
 }
