3427e3b547a3015fa8455d3e13f170707d91530e
Alexandre Guidin <alexandreguidin@gmail.com>
2020-12-09 17:25:24 -0300

    fixes

--- src/test/java/quarkus/config/BaseTest.java
+++ src/test/java/quarkus/config/BaseTest.java
@@ -1,6 +1,18 @@
 package quarkus.config;
 
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+import java.io.IOException;
+import java.util.Objects;
+
 public abstract class BaseTest {
 
-
+    public String readJson(String path) {
+        try {
+            ClassLoader classLoader = getClass().getClassLoader();
+            return new String(Objects.requireNonNull(classLoader.getResourceAsStream(path)).readAllBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
